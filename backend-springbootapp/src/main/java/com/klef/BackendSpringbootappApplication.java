751b5823a9a490f7b85e32df427832e995621244
suhas152 <vedasuhaskulkurni@gmail.com>
2025-10-18 11:17:51 +0530

    v5.0

--- backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
+++ backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
@@ -2,9 +2,10 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendSpringbootappApplication {
+public class BackendSpringbootappApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSpringbootappApplication.class, args);
